c8242d3e01f4465fa74ea22764c74f572feacb15
Valentin Scheller <valentin.scheller-vidal@epitech.eu>
2022-09-06 00:27:15 +0200

    [fix] change the structure

--- source/game_running/player_manage.c
+++ source/game_running/player_manage.c
@@ -10,34 +10,45 @@
 #include "main.h"
 #include "m_my.h"
 
+int select_move(player_t *player, int y)
+{
+    for (int x = 0; x < MAX_X; x++) {
+        if (player->move == 1 && player->direction == 0 &&
+        player->map[y][x] == 'B' && player->map[y][x - 1] == ' ') {
+            player->map[y][x] = ' ';
+            player->map[y][x - 1] = 'B';
+            return 1;
+        }
+        if (player->move == 1 && player->direction == 1 &&
+        player->map[y][x] == 'B' && player->map[y][x + 1] == ' ') {
+            player->map[y][x] = ' ';
+            player->map[y][x + 1] = 'B';
+            return 1;
+        }
+        if (player->move == 1 && player->direction == 2 &&
+        player->map[y][x] == 'B' && player->map[y - 1][x] == ' ') {
+            player->map[y][x] = ' ';
+            player->map[y - 1][x] = 'B';
+            return 1;
+        }
+        if (player->move == 1 && player->direction == 3 &&
+        player->map[y][x] == 'B' && player->map[y + 1][x] == ' ') {
+            player->map[y][x] = ' ';
+            player->map[y + 1][x] = 'B';
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int move_player(player_t *player, time_mana_t *mo)
 {
     mo->time = sfClock_getElapsedTime(mo->clock);
     mo->millisec = sfTime_asMilliseconds(mo->time);
     if (mo->millisec > 100) {
         for (int y = 0; y < MAX_Y; y++) {
-            for (int x = 0; x < MAX_X; x++) {
-                if (player->move == 1 && player->direction == 0 && player->map[y][x] == 'B' && player->map[y][x - 1] == ' ') {
-                    player->map[y][x] = ' ';
-                    player->map[y][x - 1] = 'B';
-                    return 1;
-                }
-                if (player->move == 1 && player->direction == 1 && player->map[y][x] == 'B' && player->map[y][x + 1] == ' ') {
-                    player->map[y][x] = ' ';
-                    player->map[y][x + 1] = 'B';
-                    return 1;
-                }
-                if (player->move == 1 && player->direction == 2 && player->map[y][x] == 'B' && player->map[y - 1][x] == ' ') {
-                    player->map[y][x] = ' ';
-                    player->map[y - 1][x] = 'B';
-                    return 1;
-                }
-                if (player->move == 1 && player->direction == 3 && player->map[y][x] == 'B' && player->map[y + 1][x] == ' ') {
-                    player->map[y][x] = ' ';
-                    player->map[y + 1][x] = 'B';
-                    return 1;
-                }
-            }
+            if (select_move(player, y) == 1)
+                return 1;
         }
     }
     return 0;
